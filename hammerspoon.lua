b6e2800dbe5435aa0b4542d675ca88ee68e74e53
Nithish Karthik <nithishkarthik01@gmail.com>
2023-11-22 08:50:51 +0530

    feat: added tmux and hammerspoon conf

---
+++ hammerspoon.lua
@@ -0,0 +1,9 @@
+hs.loadSpoon("AppLauncher")
+spoon.AppLauncher.modifiers = {"ctrl", "alt", "command"}
+hs.spoons.use("AppLauncher", {
+  hotkeys = {
+    i = "iTerm",
+    o = "Safari", 
+    p = "Obsidian"
+  }
+})
